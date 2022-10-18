b37966f311f98530e0e2eb895fb8a169ba67e354
Cabbage <cabbagepenetrator@gmail.com>
2022-10-18 21:46:16 +0100

    Use correct item types

--- app/Enums/ItemType.php
+++ app/Enums/ItemType.php
@@ -7,10 +7,10 @@ enum ItemType: int
     case HELMET = 0;
     case BODY = 1;
     case SHOULDER = 2;
-    case GLOVE = 3;
-    case BOOT = 4;
-    case LEFT_RING = 5;
-    case RIGHT_RING = 6;
+    case BRACERS = 3;
+    case GLOVE = 4;
+    case BOOT = 5;
+    case RING = 6;
     case AMULET = 7;
     case BELT = 8;
     case CAPE = 9;
