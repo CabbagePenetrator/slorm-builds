286cec57f23208161585c593403dfbfde4f8b423
Cabbage <cabbagepenetrator@gmail.com>
2022-10-30 01:03:44 +0100

    Create images folder

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -9,7 +9,7 @@ defineProps<{
   <button
     :type="type ?? 'submit'"
     :disabled="loading"
-    class="relative grid h-[52px] w-full place-content-center gap-x-9 border-t-8 border-b-8 border-t-red-400 border-b-red-600 bg-red-500 text-center text-lg font-normal [border-style:inset] before:absolute before:top-0 before:left-0 before:h-full before:translate-y-[-8px] before:content-[url(/button-edge.svg)] after:absolute after:top-0 after:right-0 after:h-full after:translate-y-[-8px] after:scale-x-[-1] after:content-[url(/button-edge.svg)] focus:outline focus:outline-2 focus:outline-red-400"
+    class="relative grid h-[52px] w-full place-content-center gap-x-9 border-t-8 border-b-8 border-t-red-400 border-b-red-600 bg-red-500 text-center text-lg font-normal [border-style:inset] before:absolute before:top-0 before:left-0 before:h-full before:translate-y-[-8px] before:content-[url(/images/button-edge.svg)] after:absolute after:top-0 after:right-0 after:h-full after:translate-y-[-8px] after:scale-x-[-1] after:content-[url(/images/button-edge.svg)] focus:outline focus:outline-2 focus:outline-red-400"
   >
     <div
       v-if="loading"
